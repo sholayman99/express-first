08c3762b3eeb574b7bea2540519cd1ebd4bda387
Md Sholayman <mdsholayman998@gmail.com>
2023-08-26 22:01:22 +0600

    worked on file upload and other post request

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
-
 const app = express()
 
+//middleware for parsing json data in post request
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+//---->NOTE:::==Multer is a node.js middleware for handling multipart/form-data, which is primarily used for uploading files.
+// It is written on top of busboy for maximum efficiency.
+const multer = require("multer");
+// const multers = multer();
+// app.use(multers.array());
+// app.use(express.static("public"));
 
 app.get("/", (req,res) =>{
     res.send("Hello from get route")
@@ -103,6 +112,79 @@ app.get("/eight" ,(req,res) =>{
     res.send('Setting up cookie')
 
 });
+
+
+//request method by get
+
+//query request in get api
+app.get("/query" , (req,res)=>{
+    const name = req.query.id
+    res.end(name)
+})
+
+//request by header
+app.get("/header" , (req,res)=>{
+    const name = req.header("name")
+    res.end(name)
+})
+
+//request in post method
+
+app.post("/query",(req,res)=>{
+    const id = req.query.id
+    res.end(id)
+})
+
+//post header request
+
+app.post("/header",(req,res)=>{
+    const last = req.header("last")
+    const first = req.header("first")
+    res.end(first+" "+last)
+})
+
+//json post request
+
+app.post("/json",(req,res)=>{
+   const body = req.body ;
+   const data = JSON.stringify(body);
+//    const data = JSON.stringify(name);
+   res.send(data);
+});
+
+//multiple from-data
+// app.post("/form",(req,res)=>{
+//     const formData = req.body ;
+//     res.send(formData);
+//  })
+
+ //uploading a file
+
+const storage = multer.diskStorage({
+
+destination:(req,file,callback)=>{
+ callback(null,"./upload")
+},
+
+filename:(req,file,callback)=>{
+ callback(null,file.originalname)
+}
+
+});
+
+const upload = multer({storage:storage}).single("myFile");
+
+app.post("/file",(req,res)=>{
+    upload(req,res,(err)=>{
+     if(err){
+        res.send("Failed to upload")
+     }else{
+        res.send("uploaded successfully")
+     }
+    });
+});
+ 
+
 
 app.listen(8000,()=>{
     console.log("server is running......");
